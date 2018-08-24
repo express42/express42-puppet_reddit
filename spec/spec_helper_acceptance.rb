eff8e1317519d8364096ca5207e07e559f02ba01
Nikolay Antsiferov <nklya@hotmail.com>
2018-08-24 10:14:09 +0300

    Disable modules & fix tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -14,9 +14,9 @@ PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 RSpec.configure do |c|
   c.before :suite do
     hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib')
-      on host, puppet('module', 'install', 'puppetlabs-java')
-      on host, puppet('module', 'install', 'puppet-archive')
+      # on host, puppet('module', 'install', 'puppetlabs-stdlib')
+      # on host, puppet('module', 'install', 'puppetlabs-java')
+      # on host, puppet('module', 'install', 'puppet-archive')
       copy_module_to(host, source: PROJECT_ROOT, module_name: 'jenkins')
     end
   end
